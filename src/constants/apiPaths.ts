b12f84878a94eeb8c9ea10d96aebcc4ac16a4cda
Viktoryia <viktoryia_yalavaya@epam.com>
2022-10-19 01:01:54 +0200

    task-5: Feature/Add uploading file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://jn6uf759jb.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://8nglu577mj.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://jn6uf759jb.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
